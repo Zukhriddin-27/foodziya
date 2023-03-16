230efbf936b7602b2e2cc1f5880fc9132adfca82
Zukhriddin-27 <zuxiriddin1995@gmail.com>
2023-03-16 11:06:12 +0500

    controll-acces

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 const { MONGO_URI } = require('./config/key')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
@@ -12,7 +13,10 @@ mongoose.connect(MONGO_URI, {
 
 // Add middleware function to set CORS headers
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*') // Allow any domain to access the resource
+  res.header(
+    'Access-Control-Allow-Origin',
+    'http://localhost:3000' || 'https://foodziya-web-5pti.vercel.app/'
+  ) // Allow any domain to access the resource
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
@@ -21,7 +25,7 @@ app.use(function (req, res, next) {
 })
 
 app.use(express.json())
-
+app.use(cors)
 const categoryRouter = require('./routes/category')
 const postRouter = require('./routes/post')
 // const recipeRouter = require('./routes/recipe')
