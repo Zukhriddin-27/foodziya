8598a40d4b0bd6ef86511f822a2333bea843ff34
Zukhriddin-27 <zuxiriddin1995@gmail.com>
2023-03-18 21:02:01 +0500

    bug

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
-const { MONGO_URI } = require('./config/key')
 const { db } = require('./db')
 
 db()
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-// app.all('*', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   next()
-// })
-app.use((req, res, next) => {
+app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   next()
 })
+
 app.use(express.json())
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-)
+app.use(cors())
 const categoryRouter = require('./routes/category')
 const postRouter = require('./routes/post')
 const recipeRouter = require('./routes/recipe')
